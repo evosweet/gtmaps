426b40473afcf61da651e1d2d6bfead6e6651ffc
Rayon Hunte <evosweet@gmail.com>
2017-05-03 12:13:55 -0400

    reformat ui

--- js/app.js
+++ js/app.js
@@ -20,7 +20,9 @@ $(document).ready(function () {
         var bounds = map.getBounds();
         //call google places service
         var placesService = new google.maps.places.PlacesService(map);
+
         function createMakersForPlaces(places) {
+            //init marker array 
             var bounds = new google.maps.LatLngBounds();
             for (var i = 0; i < places.length; i++) {
                 var place = places[i];
@@ -37,10 +39,20 @@ $(document).ready(function () {
                     icon: icon,
                     title: place.name,
                     position: place.geometry.location,
-                    id: place.place_id
+                    id: place.place_id,
+                    animation: google.maps.Animation.DROP
                 });
+                // add marker animation
+                function toggleBounce() {
+                    if (marker.getAnimation() !== null) {
+                        marker.setAnimation(null);
+                    } else {
+                        marker.setAnimation(google.maps.Animation.BOUNCE);
+                    }
+                }
+                marker.addListener('click', toggleBounce);
                 // create info window
-                var placesInfoWindow = new google.maps.InfoWindow()
+                var placesInfoWindow = new google.maps.InfoWindow();
                 // add event tp marker
                 marker.addListener('click', function () {
                     if (placesInfoWindow.marker == this) {
@@ -60,24 +72,57 @@ $(document).ready(function () {
             map.fitBounds(bounds);
         }
         //end
+        //get places data
+        function getPlacesDetails(marker, infowindow) {
+            var service = new google.maps.places.PlacesService(map);
+            service.getDetails({
+                placeId: marker.id
+            }, function (place, status) {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    infowindow.marker = marker;
+                    var innerHTML = '<div>';
+                    if (place.name) {
+                        innerHTML += '<strong>' + place.name + '</strong>';
+                    }
+                    if (place.formatted_address) {
+                        innerHTML += '<br>' +"Full Address "+ place.formatted_address;
+                    }
+                    if (place.formatted_phone_number) {
+                        innerHTML += '<br>' +"Phone Number "+ place.formatted_phone_number;
+                    }
+                    if (place.photos){
+                        innerHTML +='<br>'+ "Website " + place.website;
+                    }
+
+                    innerHTML += '</div>';
+                    infowindow.setContent(innerHTML);
+                    infowindow.open(map, marker);
+                } else {
+                    console.log("erorr");
+                }
+            });
+        }
+        //end
         self.setLocation = function (data) {
             console.log(data);
         };
-
+        // hide all markers
         function hideMarkers(markers) {
             for (var i = 0; i < markers.length; i++) {
                 markers[i].setMap(null);
             }
         }
+        // generate new markers from search
         self.newPlaces = function () {
-            if (!self.autoAddress()){
-                searchTeam = 'Georgetown Guyana Food'
-            }
-            else{
+            hideMarkers(self.placesMarkers);
+            self.placesMarkers.removeAll();
+            if (!self.autoAddress()) {
+                searchTeam = 'Georgetown Guyana food';
+            } else {
                 searchTeam = self.autoAddress();
             }
             placesService.textSearch({
-                query:searchTeam,
+                query: searchTeam,
                 bounds: bounds
             }, function (results, status) {
                 createMakersForPlaces(results);
