4c3327099adb8e227c0ab94e8d139b11f4bc9b44
Rayon Hunte <evosweet@gmail.com>
2017-05-04 10:13:51 -0400

    code clean up

--- js/app.js
+++ js/app.js
@@ -7,6 +7,14 @@ $(document).ready(function () {
         self.autoAddress = ko.observable("");
         //init places array
         self.placesMarkers = ko.observableArray([]);
+
+        //
+        self.placeTitle = ko.observable();
+        self.placeAddress = ko.observable();
+        self.placePhone = ko.observable();
+        self.placeWebsite = ko.observable();
+        self.urlTitle = ko.observable();
+        //
         //init map
         var map = new google.maps.Map(document.getElementById('map'), {
             zoom: 13,
@@ -42,26 +50,17 @@ $(document).ready(function () {
                     id: place.place_id,
                     animation: google.maps.Animation.DROP
                 });
-                // add marker animation
-                function toggleBounce() {
-                    if (marker.getAnimation() !== null) {
-                        marker.setAnimation(null);
-                    } else {
-                        marker.setAnimation(google.maps.Animation.BOUNCE);
-                    }
-                }
-                marker.addListener('click', toggleBounce);
                 // create info window
                 var placesInfoWindow = new google.maps.InfoWindow();
+                // toggle marker
+                // add marker animation
                 // add event tp marker
                 marker.addListener('click', function () {
-                    if (placesInfoWindow.marker == this) {
-                        console.log("this infowindow already is on this ,marker");
-                    } else {
-                        getPlacesDetails(this, placesInfoWindow);
-                    }
+                    map.setCenter(this.getPosition());
+                    getPlacesDetails(this, placesInfoWindow);
+                    toggleBounce(this);
                 });
-                //push marker data
+
                 self.placesMarkers.push(marker);
                 if (place.geometry.viewport) {
                     bounds.union(place.geometry.viewport);
@@ -72,9 +71,20 @@ $(document).ready(function () {
             map.fitBounds(bounds);
         }
         //end
+        function toggleBounce(marker) {
+            if (marker.getAnimation() !== null) {
+                marker.setAnimation(null);
+            } else {
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+                setTimeout(function(){ marker.setAnimation(null); }, 1000);
+            }
+        }
+
         //get places data
         function getPlacesDetails(marker, infowindow) {
             var service = new google.maps.places.PlacesService(map);
+            self.getDetails(marker);
+            console.log();
             service.getDetails({
                 placeId: marker.id
             }, function (place, status) {
@@ -82,16 +92,16 @@ $(document).ready(function () {
                     infowindow.marker = marker;
                     var innerHTML = '<div>';
                     if (place.name) {
-                        innerHTML += '<strong>' + place.name + '</strong>';
+                        innerHTML += '<strong>' + self.placeTitle() + '</strong>';
                     }
                     if (place.formatted_address) {
-                        innerHTML += '<br>' +"Full Address "+ place.formatted_address;
+                        innerHTML += '<br>' + "Full Address " + self.placeAddress();
                     }
                     if (place.formatted_phone_number) {
-                        innerHTML += '<br>' +"Phone Number "+ place.formatted_phone_number;
+                        innerHTML += '<br>' + "Phone Number " + self.placePhone();
                     }
-                    if (place.photos){
-                        innerHTML +='<br>'+ "Website " + place.website;
+                    if (place.website) {
+                        innerHTML += '<br>' + "Website " + place.website;
                     }
 
                     innerHTML += '</div>';
@@ -103,8 +113,31 @@ $(document).ready(function () {
             });
         }
         //end
-        self.setLocation = function (data) {
-            console.log(data);
+
+        //places services
+        self.getDetails = function(marker){
+            service = new google.maps.places.PlacesService(map);
+            service.getDetails({
+                placeId: marker.id
+            },function(place, status){
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                   self.placeTitle(place.name); 
+                   self.placeAddress(place.formatted_address);
+                   self.placePhone(place.formatted_phone_number);
+                   if (place.website){
+                        self.placeWebsite(place.website);
+                        self.urlTitle("Click On Me");
+                   }else{
+                        self.urlTitle("No Link");
+                   }
+                }
+            });
+        };
+
+        self.setLocation = function (marker) {
+            self.getDetails(marker);
+            map.setCenter(marker.getPosition());
+            toggleBounce(marker);
         };
         // hide all markers
         function hideMarkers(markers) {
